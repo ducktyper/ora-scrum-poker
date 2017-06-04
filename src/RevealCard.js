29600cfc5dd33a3167ee5ee63e31a9cac94efec6
Ducksan Cho <ducktyper@gmail.com>
2017-06-04 22:13:43 +1200

    Style revealed value

--- src/RevealCard.js
+++ src/RevealCard.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import './RevealCard.css';
 
 class RevealCard extends Component {
+  displayClass() {
+    if (this.props.choice === 'coffee') return 'size-coffee'
+
+    return `size-${this.props.choice.length}`;
+  }
+
+  displayText() {
+    if (this.props.choice === 'coffee') return '';
+
+    return this.props.choice;
+  }
+
   render() {
     return (
-      <div className='reveal-card size-1' onClick={() => this.props.reset()}>
-        {this.props.choice}
+      <div className={`reveal-card ${this.displayClass()}`} onClick={() => this.props.reset()}>
+        {this.displayText()}
       </div>
     );
   }
