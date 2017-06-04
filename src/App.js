860f72e5dbeca144c3090300880e3524be049e34
Ducksan Cho <ducktyper@gmail.com>
2017-06-04 21:17:50 +1200

    Add ReadyToReveal component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import SelectBox from './SelectBox.js';
+import ReadyToReveal from './ReadyToReveal.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <SelectBox />
+      <div className='app'>
+        <SelectBox />
+        <ReadyToReveal />
+      </div>
     );
   }
 }
---
+++ src/ReadyToReveal.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './ReadyToReveal.css';
+
+class ReadyToReveal extends Component {
+  render() {
+    return (
+      <div className='ready-to-reveal'>
+        Ready
+      </div>
+    );
+  }
+}
+
+export default ReadyToReveal;
